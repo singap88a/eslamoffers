9a964f29c3fb20e41c2fb2a396be0198994719a4
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2025-08-22 02:38:03 +0300

    server.js_1

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const { createServer } = require('http');
-const next = require('next');
-
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+ 
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res);
-  }).listen(process.env.PORT || 3000, (err) => {
-    if (err) throw err;
-    console.log(🚀 Next.js app ready on port ${process.env.PORT || 3000});
-  });
-});
+	const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl)
+  }).listen(port)
+ 
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
